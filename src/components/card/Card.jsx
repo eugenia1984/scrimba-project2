bcdf258f8042df249bd7f5c0ea8c23449738a80d
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-21 15:33:36 -0300

    sigo con las cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import  CardInfo   from "./CardInfo.js";
 import "./CardStyles.css";
+import "../../App.css";
 
 function Card() {
+  
   return (
-    <section className="cards">
-      card
-    </section>
+    { CardInfo.map( card => {
+      return (
+        <div className="card" key={ card.id }>
+          <img 
+            className="card-image" 
+            src={ card.imagesrc }
+            alt= {card.p1 }/>
+          <div className="card-stats">
+            <img 
+              className="card-star"
+              src="https://img.icons8.com/color/14/null/hand-drawn-star.png" 
+              alt="star"/>
+            <span>{card.rating}</span>
+            <span className="gray">( {card.reviewCount} ) *</span>
+            <span className="gray">{card.country}</span>
+          </div>
+          <p>{card.title}</p>
+          <p><span className="bold">From $ {card.price} </span>/ person</p>
+        </div>
+      )
+    })}
   );
 }
 
---
+++ src/components/card/CardInfo.js
@@ -0,0 +1,32 @@
+export const CardInfo = [
+  {
+    id: 1,
+    imagesrc: "",
+    rating: "5.0",
+    reviewCount: 7,
+    country: "USA",
+    title: "Life Lessons with Katie Zaferes",
+    price: 135
+    
+  },
+  {
+    id: 2,
+    imagesrc: "",
+    rating: "5.0",
+    reviewCount: 6,
+    country: "USA",
+    title: "Life Lessons with Katie Zaferes",
+    price: 135
+    
+  },
+  {
+    id: 3,
+    imagesrc: "",
+    rating: "5.0",
+    reviewCount: 6,
+    country: "USA",
+    title: "Life Lessons with Katie Zaferes",
+    price: 135,
+    
+  }
+];
