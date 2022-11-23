128923dab851c75ff07fd7c6ee6ac8adef965a61
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-23 18:48:03 -0300

    creo data.js para simular que traemos la informacion de una API

---
+++ src/data.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    title: "Life Lessons with Katie Zaferes",
+    description: "I will share with you what I call Positively Impactful Moments of Disappointment. Throughout my career, many of my higest moments only come after setbacks and tosses. But learning from those difficult moments is what gave me the ability to rise above and reach my goals.",
+    price: 135,
+    coverImg: "../../assets/swimmer.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 7
+    },
+    location: "Online",
+    openSpots: 0,
+  },
+  {
+    id: 2,
+    title: "Learn wedding photography",
+    description: "Interested in becaming a wedding photographer? For beginner and experienced photographeres alike, join us in techniques required to leave the happy couple with memories that´ll last a lifetime",
+    price: 125,
+    coverImage: "../../assets/bride-smiling.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 30
+    },
+    location: "Online",
+    spots: 0,  
+  },
+  {
+    id: 3,
+    title: "Group Mountain Biking",
+    description: "",
+    price: 50,
+    coverImg: "../../assets/mountain-bike.jpg",
+    stats: {
+      rating: 4.8,
+      reviewCount: 2
+    },
+    location: "Norway",
+    openSpots: 3,  
+  }
+];
