71100309e579ecd41d71f86fb360b7b83a3e068e
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-11-23 18:37:00 -0300

    agrego las imagenes para las cards en assets

--- src/components/card/CardInfo.js
+++ src/components/card/CardInfo.js
@@ -1,8 +1,8 @@
 export const CardInfo = [
   {
     id: 1,
-    imagesrc: "",
-    rating: "5.0",
+    imagesrc: "../../assets/swimmer.jpg",
+    rating: 5.0,
     reviewCount: 7,
     country: "USA",
     title: "Life Lessons with Katie Zaferes",
@@ -11,22 +11,22 @@ export const CardInfo = [
   },
   {
     id: 2,
-    imagesrc: "",
-    rating: "5.0",
-    reviewCount: 6,
+    imagesrc: "../../assets/bride-smiling.jpg",
+    rating: 5.0,
+    reviewCount: 30,
     country: "USA",
-    title: "Life Lessons with Katie Zaferes",
-    price: 135
+    title: "Learn wedding photography",
+    price: 125
     
   },
   {
     id: 3,
-    imagesrc: "",
-    rating: "5.0",
-    reviewCount: 6,
+    imagesrc: "../../assets/mountain-bike.jpg",
+    rating: 4.8,
+    reviewCount: 2,
     country: "USA",
-    title: "Life Lessons with Katie Zaferes",
-    price: 135,
+    title: "Group Mountain Biking",
+    price: 50,
     
   }
 ];
